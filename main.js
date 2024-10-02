a8697c530f4b7b3fc2bb4c06b58bc271f58643f7
Maria Aluko <maria.alukoo@gmail.com>
2024-10-02 14:34:20 +0300

    add first step comments

--- main.js
+++ main.js
@@ -17,8 +17,10 @@ class Car {
 }
 
 const addCar = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // this prevents form submission until submit is actually clicked
 
+    // use .value.trim(); in order to prevent accidental whitespaces entered
+    // define the input value for each form input as a variable:
     const license = document.querySelector('#license').value.trim();
     const maker = document.querySelector('#maker').value.trim();
     const model = document.querySelector('#model').value.trim();
@@ -26,42 +28,52 @@ const addCar = (e) => {
     const price = document.querySelector('#price').value.trim();
     const color = document.querySelector('#color').value;
 
+    // create a object with the parameters
+    // since the variables and parameters are called same, this is a shortcut: otherwise use constructor with .this
     const newCar = new Car(license, maker, model, owner, price, color);
 
+    // resets the values of all elements in a form (same as clicking the Reset button):
     addCarForm.reset();
-
+    // push the new Car object to the Cars array:
     cars.push(newCar)
+    // run the displayTable function to display the entered data in table:
     displayTable();
 }
 
 const displayTable = () => {
-    const table = document.querySelector('#carsTable');
+    const table = document.querySelector('#carsTable'); //define table with id from html
 
-    table.innerHTML = table.rows[0].innerHTML;
+    table.innerHTML = table.rows[0].innerHTML; //
 
     cars.forEach(car => {
-        const row = table.insertRow(-1);
+        const row = table.insertRow(-1); // for each car in the array, insert a row into the table
 
         Object.values(car).forEach(text => {
-            const cell = row.insertCell(-1);
-            cell.textContent = text;
+            const cell = row.insertCell(-1); // for each car object value, insert a cell to the row
+            cell.textContent = text; // put the text value into each cell created
         })
     })
 }
 
 const searchCar = (e) => {
-    e.preventDefault();
-    const searchInput = document.querySelector('#search').value;
+    e.preventDefault(); // this prevents search until Search is actually clicked
+    const searchInput = document.querySelector('#search').value; // get the value writen by user as a variable
+
+    // take the Cars array, find if any object (car) with a license parameter is equal to the search
+    // put it as a variable
+    // put both to lowerCase to avoid upper and lower case differences:
     const foundCar = cars.find(car => car.license.toLowerCase() === searchInput.toLowerCase());
 
     const searchResult = document.querySelector('#searchResult');
     if (foundCar) {
+        // if the foundCar value was found, set the text of the p with searchResult equal to:
+        // take each parameter and place it as text:
         searchResult.innerHTML = `
           <p>Maker: ${foundCar.maker}</p>
           <p>Model: ${foundCar.model}</p>
           <p>Owner: ${foundCar.owner}</p>
           <p>Price: ${foundCar.price.toFixed(2)}€</p>
-        `;
+        `; // the last line turns the numerical value to a specified number of decimal places, here 2
     } else {
         searchResult.innerHTML = '<p>No car found with the given license plate.</p>';
     }
